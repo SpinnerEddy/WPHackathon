01e553e98ed434b5f4d822f54cf911023103c349
Unique22 <tales.wrm.rl2102@icloud.com>
2018-02-27 19:05:01 +0900

    物体を追従するスクリプトの作成

---
+++ WPHackathon/Assets/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject following;
+
+    private Vector3 cameraPosition;
+
+	// Use this for initialization
+	void Start () {
+        cameraPosition = new Vector3(0, 1, -10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = following.transform.position + cameraPosition;
+	}
+}
