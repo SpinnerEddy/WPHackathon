4049066f0dd76f6d803318d810446fc07528db42
MYCOMPUTER\8oo36 <568p9e30t642@gmail.com>
2018-02-27 19:58:35 +0900

    障害物の生成（仮）

---
+++ WPHackathon/Assets/GameMain/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    private float _distants;
+    private float _height;
+
+    // Use this for initialization
+    public void Start ()
+    {
+        _distants = 0;
+        _height = 1000;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void HeightUpDwon(float d)
+    {
+        _height -= d;
+
+    }
+}
