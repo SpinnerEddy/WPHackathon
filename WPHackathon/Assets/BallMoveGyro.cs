ece99ef5d67112a364affb982f7a8d0293fd96da
Unique22 <tales.wrm.rl2102@icloud.com>
2018-02-27 19:14:28 +0900

    ジャイロセンサーのテスト

---
+++ WPHackathon/Assets/BallMoveGyro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveGyro : MonoBehaviour {
+
+    private float speed;
+
+    private Vector3 direct;
+
+	// Use this for initialization
+	void Start () {
+        speed = 10.0f;
+        direct = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direct = Vector3.zero;
+
+        direct.x = Input.acceleration.x;
+        direct.z = Input.acceleration.z;
+
+        if(direct.sqrMagnitude > 1){
+            direct.Normalize();
+        }
+
+        direct *= Time.deltaTime;
+
+        this.transform.Translate(direct * speed);
+	}
+}
