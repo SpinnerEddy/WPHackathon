b093c9a24a260ae48ca1169d9021edb561da163a
wakkey <oingoboingo3911@yahoo.co.jp>
2018-02-27 22:00:21 +0900

    大まかなレイアウト設定&ボタン処理

---
+++ WPHackathon/Assets/Scenes/Select/SelectedPlane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedPlane : MonoBehaviour {
+    public static int selectedID = -1;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+    }
+    public static void selectPlane(int num)
+    {
+        selectedID = num;
+    }
+	
+}
