ff2f6233316c98b654581ed5296fafbf3264fe68
Yuki <yutaroud@yahoo.co.jp>
2018-02-27 20:31:33 +0900

    タイトルの設定

---
+++ WPHackathon/Assets/AirPlane.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirPlane : MonoBehaviour {
+
+	private float speed = -0.3f;    //これを追加
+
+	// Use this for initialization
+	void Start () {
+		//以下を追加
+
+		this.GetComponent<Rigidbody>().AddForce(
+			(transform.forward + transform.right) * speed,
+			ForceMode.VelocityChange);
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
